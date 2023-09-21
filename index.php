d479814536af1ca0a2a7299e3e8e990ec3d019b1
Lmat123 <118364975+Lmat123@users.noreply.github.com>
2023-09-21 09:17:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="data.php" method="post">
+
+Name: <input type="text" name="name"><br>
+LastName: <input type="text" name="lastname"><br>
+Input Password <input type="text" name="password"><br>
+
+<input type="submit">
+
+</form>
+
+</body>
+</html>
